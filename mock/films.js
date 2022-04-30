d460078c4261bba4d8cbd9813aededbf9578c4ec
Valentin Orlov <valentin_orlov@epam.com>
2022-04-30 14:44:05 +0300

    2.9.1.Заведите структуру данных, которая опишет фильм

---
+++ mock/films.js
@@ -0,0 +1,69 @@
+import {getRandomInteger} from '../utils.js';
+
+const generateDescription = () => {
+  const descriptions = [
+    'Oscar-winning film, a war drama about two young people, from the creators of timeless classic "Nu, Pogodi!" and "Alice in Wonderland", with the best fight scenes since Bruce Lee.',
+    'Cumsan turpis nec elit congue, sit amet aliquet felis dapibus. Mauris auctor ornare tellus. Donec maximus quis nunc in sollicitudin. Quisqu…',
+    'Curabitur lacinia, lacus a egestas auctor, massa enim commodo elit, neque mauris a nunc. Donec ipsum felis, ve facilisis tortor commodo etc…',
+  ];
+
+  const randomIndex = getRandomInteger(0, descriptions.length - 1);
+
+  return descriptions[randomIndex];
+};
+const generateFilmsTitle = () => {
+  const filmsTitle = [
+    'Popeye the Sailor Meets Sindbad the Sailor',
+    'Sagebrush Trail',
+    'The Dance of Life',
+  ];
+
+  const randomIndex = getRandomInteger(0, filmsTitle.length - 1);
+
+  return filmsTitle[randomIndex];
+};
+
+export const generateFilms = () => ({
+  id: '0',
+  comments: [
+    1, 2
+  ],
+  filmInfo: {
+    title: generateFilmsTitle(),
+    alternativeTitle: 'Laziness Who Sold Themselves',
+    totalRating: 5.3,
+    poster: 'images/posters/blue-blazes.jpg',
+    ageRating: 0,
+    director: 'Tom Ford',
+    writers: [
+      'Takeshi Kitano'
+    ],
+    actors: [
+      'Morgan Freeman'
+    ],
+    release: {
+      date: '2019-05-11T00:00:00.000Z',
+      releaseCountry: 'Finland'
+    },
+    runtime: 77,
+    genre: [
+      'Comedy'
+    ],
+    description: generateDescription()
+  },
+  userDetails: {
+    watchlist: false,
+    alreadyWatched: true,
+    watchingDate: '2019-04-12T16:12:32.554Z',
+    favorite: false
+  }
+});
+
+
+export const generateComments = () =>({
+  id: '42',
+  author: 'Ilya O\'Reilly',
+  comment: 'a film that changed my life, a true masterpiece, post-credit scene was just amazing omg.',
+  date: '2019-05-11T16:12:32.554Z',
+  emotion: 'smile'
+});
